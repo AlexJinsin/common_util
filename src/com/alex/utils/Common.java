1546aa7d5f9ab154da9333a1b2e1d51aef7758a2
Alex <569486554@qq.com>
2016-05-11 10:19:10 +0800

    增加了个设置默认值的方法

--- src/com/alex/utils/Common.java
+++ src/com/alex/utils/Common.java
@@ -1,9 +1,24 @@
 package com.alex.utils;
 
+
 public class Common {
 
 	public Common() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	/**
+	 * @description:用于 设置请求 中空值 的默认值
+	 * @param req
+	 * @param parameter 一个参数
+	 * @param defaultValue
+	 * @return:
+	 * @time: 下午6:00:47
+	 * @author: alex
+	 */
+	public static String setDefaultVale(final String parameter,final String defaultValue) {
+		
+		return (org.apache.commons.lang3.StringUtils.isBlank(parameter)) ? defaultValue: parameter;
+    }
 
 }
