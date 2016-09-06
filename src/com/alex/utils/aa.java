4764570882a5b656b2aa6e3c8dff7cd41224e06d
Alex <569486554@qq.com>
2016-09-06 11:05:08 +0800

    正则表达式
    增加了一个正则表达式的demo

---
+++ src/com/alex/utils/aa.java
@@ -0,0 +1,25 @@
+package com.alex.utils;
+
+public class aa {
+
+	public aa() {
+		// TODO Auto-generated constructor stub
+//		String processes = Terminal.sendShortCommandLine(Constants.ADB + " -s " + device.getSerialnum() + " shell ps");
+//		Matcher matcher = Pattern.compile("^\\w+\\s+(\\d+)\\s.*" + PROCESS_NAME + "$", Pattern.MULTILINE).matcher(processes);
+//		if(matcher.find()) {
+//			String pid = matcher.group(1);
+//			String result = Terminal.sendShortCommandLine(Constants.ADB + " -s " + device.getSerialnum() + " shell \"kill -9 " + pid + "\"");
+//			logger.info("device id:" + device.getId() + " Serialnum:" + device.getSerialnum() + " Kill suserver pid=" + pid + ":" + result);
+//		}
+//		
+//		processes = Terminal.sendShortCommandLine(Constants.ADB + " -s " + device.getSerialnum() + " shell ps");
+//		Matcher subMatcher = Pattern.compile("^\\w+\\s+(\\d+)\\s.*" + SUB_PROCESS_NAME + "$", Pattern.MULTILINE).matcher(processes);
+//		while(subMatcher.find()) {
+//			String pid = subMatcher.group(1);
+//			String result = Terminal.sendShortCommandLine(Constants.ADB + " -s " + device.getSerialnum() + " shell \"kill -9 " + pid + "\"");
+//			logger.info("device id:" + device.getId() + " Serialnum:" + device.getSerialnum() + " Kill suserver pid=" + pid + ":" + result);
+//		}
+	}
+	
+
+}
